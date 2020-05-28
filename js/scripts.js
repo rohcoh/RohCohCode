bf2e35905942b15bd2935e159fcf6334636eee27
rohcoh <rcsulfur@gmail.com>
2020-05-28 13:02:47 -0400

    Added in description text for items

--- js/scripts.js
+++ js/scripts.js
@@ -52,27 +52,27 @@
       
       [{
         src:"img/bday_gd/gd0.png",
-        title:"<br><a target='_blank' href='https://www.bluemountain.com/ecards/birthday/beautiful-birthday-garden-interactive/card-3509034'>Link to card</a>"
+        title:"New interact Birthday card made for the 2020 year. Main programmer on project. Worked with the artist to put in animations and various art assets. tasked with ptimization for older devices and handling particles. Made using Typscript with Greensock, PIXIJS, and inhouse libraries. <br><a target='_blank' href='https://www.bluemountain.com/ecards/birthday/beautiful-birthday-garden-interactive/card-3509034'>Link to card</a>"
       },
       {
         src:"img/bday_gd/gd1.png",
-        title:"e"
+        title:"Main scene."
       },
       {
         src:"img/bday_gd/gd2.png",
-        title:"e"
+        title:"Animated name ending made using name pathfinder to spell out the recipient's name in confetti. Balloons float in from the bottom of the screen."
       },
       {
         src:"img/bday_gd/gd3.png",
-        title:"e"
+        title:"Splash screen when exitting fullscreen."
       }],
       [{
         src:"img/sd_md/md0.png",
-        title:"Spot the difference game made for Mother's day 2020. Collaberated programming with one other coworker. Worked on updating programming framework, particle effects, implamenting audio, and putting in new art assets. <br><a target='_blank' href='https://www.bluemountain.com/ecards/mothers-day/mothers-day-spot-the-differences-game/card-3506711'>Link to game</a>"
+        title:"Spot the difference game made for Mother's Day 2020. Collaberated programming with one other coworker. Worked on updating programming framework, particle effects, implamenting audio, and putting in new art assets. Made using Typscript with Greensock, PIXIJS, and inhouse libraries. <br><a target='_blank' href='https://www.bluemountain.com/ecards/mothers-day/mothers-day-spot-the-differences-game/card-3506711'>Link to game</a>"
       },
       {
         src:"img/sd_md/md1.png",
-        title:"Main game scene"
+        title:"Main game scene."
       },
       {
         src:"img/sd_md/md2.png",
@@ -80,33 +80,33 @@
       },
       {
         src:"img/sd_md/md3.png",
-        title:"e"
+        title:"Ending scene. Displays fastest time."
       }],
 
       [{
         src:"img/cmas_2019/cm0.png",
-        title:"<br><a target='_blank' href='https://www.bluemountain.com/ecards/christmas/the-gift-of-christmas-personalize/card-3491452'>Link to card</a>"
+        title:"Interactive card made for Christmas 2020. Main programmer on project. Worked with the artist to put in animations and various art assets. Tasked with optimization for older devices and handling particles. Made using Typscript with Greensock, PIXIJS, and inhouse libraries. <br><a target='_blank' href='https://www.bluemountain.com/ecards/christmas/the-gift-of-christmas-personalize/card-3491452'>Link to card</a>"
       },
       {
         src:"img/cmas_2019/cm1.png",
-        title:"e"
+        title:"Ending custom name screen"
       },
       {
         src:"img/cmas_2019/cm2.png",
-        title:"e"
+        title:"Splash screen when exitting fullscreen."
       }],
 
       [{
         src:"img/halloween/h0.png",
-        title:"<br><a target='_blank' href='https://www.bluemountain.com/ecards/halloween/halloween-hidden-objects-game/card-3501609'>Link to game</a>"
+        title:" Hidden object game made for Halloween 2019. Collaberated programming with two other programmers. Worked on optimizing spritesheet, adding in and adjusting audio, animations, and particle effects. Made using Typscript with Greensock, PIXIJS, and inhouse libraries.<br><a target='_blank' href='https://www.bluemountain.com/ecards/halloween/halloween-hidden-objects-game/card-3501609'>Link to game</a>"
       },
       {
         src:"img/halloween/h1.png",
-        title:"e"
+        title:"Main game scene."
       },
       {
         src:"img/halloween/h2.png",
-        title:"e"
+        title:"Ending scene. Animation plays before showing time or telling player to play again."
       }],
             
       [{
@@ -129,11 +129,11 @@
             
       [{
         src:"img/kts/kts0.png",
-        title:"<br><a target='_blank' href='https://killthestreamer.itch.io/kill-the-streamer'>Link to game</a>"
+        title:"Game made for RIT production studio class in 2016. Collaberated with 3-4 other programmers and 1 artist. Game connects to twitch using the twitch api with Unity. When user types in certain emotes into chat that emote's enemy version will spawn in game. Tasked with ai design, ai programming, level design and collaberated with gun programming. Made in Unity. <br><a target='_blank' href='https://killthestreamer.itch.io/kill-the-streamer'>Link to game</a>"
       },
       {
         src:"img/kts/kts1.png",
-        title:"e"
+        title:"Main game scene. FrankerZ emotes will club together, pause and then launch in a circular pattern around the player. TheIlluminati emotes will charge at the player and will run away if the player looks at them."
       },
       {
         src:"img/kts/kts2.png",
